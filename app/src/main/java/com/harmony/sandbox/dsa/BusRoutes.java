a5e7d9b58c676e2e456f51df4b44244b4e6e3cd0
Eric Njogu <kunadawa@gmail.com>
2023-12-08 12:04:22 +0300

    dsa: implementing bus routes - passing 11 tests on leetcode

--- app/src/main/java/com/harmony/sandbox/dsa/BusRoutes.java
+++ app/src/main/java/com/harmony/sandbox/dsa/BusRoutes.java
@@ -2,40 +2,95 @@ package com.harmony.sandbox.dsa;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Queue;
+import java.util.Set;
 
 @Slf4j
 public class BusRoutes {
     public int numBusesToDestination(int[][] routes, int source, int target) {
+        // special bus to denote first visit to source
+        final int ON_FOOT = -1;
         // create map with graph nodes
         Map<Integer, List<Stop>> routeStops = new HashMap<>();
         int bus = 0;
         for (int[] route: routes) {
             for (int i = 0; i < route.length; i++) {
-                log.debug("bus {}, stop {}", bus, route[i]);
+                //log.debug("bus {}, stop {}", bus, route[i]);
                 List<Stop> stops = routeStops.get(route[i]);
                 if (Objects.isNull(stops)) {
                     stops = new ArrayList<>();
                     routeStops.put(route[i], stops);
                 }
                 int next = i + 1;
+                int prev = i - 1;
                 if (next >= route.length) {
-                    // no further stops
+                    // no further stop
                 } else {
                     stops.add(new Stop(route[next], bus));
+                }
+                if (prev < 0) {
+                    // no previous stop
+                } else {
+                    stops.add(new Stop(route[prev], bus));
                 }
             }
             bus += 1;
         }
-        log.debug("sanity {}", routeStops);
+        // sort route stops by increasing order of stop id so that earlier stops are found first
+        for (Integer stopId: routeStops.keySet()) {
+            routeStops.put(stopId, routeStops.get(stopId).stream().sorted(
+                    Comparator.comparingInt(s -> s.id)
+            ).toList());
+        }
+        log.debug("route stops sorted: {}", routeStops);
         // initialise queue
-
+        Queue<Stop> queue = new ArrayDeque<>();
+        // initialise visited stops
+        List<Stop> visitedStops = new ArrayList<>();
+        visitedStops.add(new Stop(source, ON_FOOT));
         // do a breadth-first search
-        return -1;
+        queue.addAll(routeStops.get(source));
+        boolean found = false;
+        while (Boolean.FALSE.equals(queue.isEmpty())) {
+            Stop currentStop = queue.poll();
+            if (visitedStops.contains(currentStop)) {
+                continue;
+            }
+            visitedStops.add(currentStop);
+            if (currentStop.id == target) {
+                found = true;
+                break;
+            } else {
+                // add stops, starting by those that match the bus used last
+                int currentBus = currentStop.bus;
+                List<Stop> sortedStops = routeStops.get(currentStop.id).stream().sorted(
+                        Comparator.comparingInt(s -> Math.abs(s.bus - currentBus))
+                ).toList();
+                log.debug("sorted stops: {}", sortedStops);
+                queue.addAll(sortedStops);
+            }
+            log.debug("visited: {}", visitedStops);
+            log.debug("queue: {}", queue);
+        }
+        if (found) {
+            Set<Integer> buses = new HashSet<>();
+            for (Stop visitedStop: visitedStops) {
+                buses.add(visitedStop.bus);
+            }
+            log.debug("buses: {}", buses);
+            // do not count getting on the bus at the first stop
+            return buses.size() - 1;
+        } else {
+            return -1;
+        }
     }
     record Stop (int id, int bus) {}
 }
--- app/src/test/java/com/harmony/sandbox/dsa/BusRoutesTest.java
+++ app/src/test/java/com/harmony/sandbox/dsa/BusRoutesTest.java
@@ -20,4 +20,50 @@ class BusRoutesTest {
     void numBusesToDestination_01() {
         assertEquals(2, busRoutes.numBusesToDestination(new int[][]{{1,2,7},{3,6,7}}, 1, 6));
     }
+
+    @Test
+    @DisplayName("when a route does not exist")
+    void numBusesToDestination_02() {
+        assertEquals(-1, busRoutes.numBusesToDestination(
+                new int[][]{{7,12},{4,5,15},{6},{15,19},{9,12,13}}, 15, 12));
+    }
+
+    @Test
+    @DisplayName("when a route exists on same bus")
+    void numBusesToDestination_03() {
+        assertEquals(1, busRoutes.numBusesToDestination(
+                new int[][]{{24},
+                        {3,6,11,14,22},
+                        {1,23,24},
+                        {0,6,14},
+                        {1,3,8,11,20}}, 20, 8));
+    }
+
+    @Test
+    @DisplayName("when a shorter route exists on same bus")
+    void numBusesToDestination_04() {
+        assertEquals(1, busRoutes.numBusesToDestination(
+                new int[][]{{1,9,12,20,23,24,35,38},
+                        {10,21,24,31,32,34,37,38,43},
+                        {10,19,28,37},
+                        {8},
+                        {14,19},
+                        {11,17,23,31,41,43,44},
+                        {21,26,29,33},
+                        {5,11,33,41},
+                        {4,5,8,9,24,44}}, 37, 28));
+    }
+
+    @Test
+    @DisplayName("when a shorter route exists on same bus")
+    void numBusesToDestination_05() {
+        assertEquals(1, busRoutes.numBusesToDestination(
+                new int[][]{{0,1,6,16,22,23},
+                        {14,15,24,32},
+                        {4,10,12,20,24,28,33},
+                        {1,10,11,19,27,33},
+                        {11,23,25,28},
+                        {15,20,21,23,29},
+                        {29}}, 4, 21));
+    }
 }
