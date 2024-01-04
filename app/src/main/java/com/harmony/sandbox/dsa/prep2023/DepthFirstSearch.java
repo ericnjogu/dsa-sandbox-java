5e9c44331d3fed008aed6a9cfdb2e9696ede24f8
Eric Njogu <kunadawa@gmail.com>
2024-01-04 07:44:02 +0300

    adding undirected path search for debugging

--- app/src/main/java/com/harmony/sandbox/dsa/prep2023/DepthFirstSearch.java
+++ app/src/main/java/com/harmony/sandbox/dsa/prep2023/DepthFirstSearch.java
@@ -3,10 +3,14 @@ package com.harmony.sandbox.dsa.prep2023;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * depth first search
@@ -35,5 +39,35 @@ public class DepthFirstSearch {
         }
 
         return visited;
+    }
+
+    public boolean undirectedPath(List<List<String>> edges, String nodeA, String nodeB) {
+        // convert to adjacency list
+        Map<String, List<String>> graph = new HashMap<>();
+        for (List<String> edge: edges) {
+            graph.computeIfAbsent(edge.get(0), list -> new ArrayList<>()).add(edge.get(1));
+            graph.computeIfAbsent(edge.get(1), list -> new ArrayList<>()).add(edge.get(0));
+        }
+        System.out.println("graph is " + graph);
+        // use depth first search to check
+        Deque<String> stack = new ArrayDeque<>();
+        stack.push(nodeA);
+        Set<String> visited = new HashSet<>();
+        while (!stack.isEmpty()) {
+            String node = stack.pop();
+            visited.add(node);
+            if (node.equals(nodeB)) {
+                System.out.println("returning true");
+                return true;
+            } else {
+                for (String neighbor: graph.get(node)) {
+                    if (! visited.contains(neighbor)) {
+                        stack.push(neighbor);
+                    }
+                }
+            }
+        }
+        System.out.println("returning false");
+        return false;
     }
 }
--- app/src/test/java/com/harmony/sandbox/dsa/prep2023/DepthFirstSearchTest.java
+++ app/src/test/java/com/harmony/sandbox/dsa/prep2023/DepthFirstSearchTest.java
@@ -21,4 +21,16 @@ class DepthFirstSearchTest {
     void dfs() {
         assertEquals(List.of("a", "b", "d", "c", "e"), dfs.search(TestData.getStructyGraph_01(), "a"));
     }
+
+    @Test
+    void undirectedPath_01() {
+        List<List<String>> edges = List.of(
+                List.of("i", "j"),
+                List.of("k", "i"),
+                List.of("m", "k"),
+                List.of("k", "l"),
+                List.of("o", "n")
+        );
+        assertTrue(dfs.undirectedPath(edges, "j", "m"));
+    }
 }
